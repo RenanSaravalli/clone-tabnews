c1287c32a0a777260acd0dc2bbd3c5e6dfaedb44
RenanSampaioSaravalli <renan.saravalli@aluno.ifsp.edu.br>
2026-04-18 17:56:29 -0300

    Finaliza o endpoint de

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,32 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
+  const updateAt = new Date().toISOString();
 
-  response
-    .status(200)
-    .json({ message: "Alunos do curso.dev são pessoas acima da média" });
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const openedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const openedConnections = openedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connection: parseInt(maxConnections),
+        opened_connections: openedConnections,
+      },
+    },
+  });
 }
 export default status;
